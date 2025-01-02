e077e790a0b5b4c5a28692e628d04979e48bb40c
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-01-01 22:44:58 -0300

    commit-006

---
+++ public/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout-comp/index";
+import { UI } from "./ui-page";
+
+export default function Home() {
+  return (
+    <Layout>
+      <UI />
+    </Layout>
+  );
+}
